5d7a1717747e7263e6148db8aec6f675e5196d70
Scott Bussinger <scottb@bussinger.org>
2012-06-21 10:28:08 -0700

    Updated for iTag 483
    Updated to installer version 483
    Updated the licence URL and description

--- itag/tools/chocolateyInstall.ps1
+++ itag/tools/chocolateyInstall.ps1
@@ -1,1 +1,1 @@
-Install-ChocolateyPackage 'itag' 'exe' '/S' 'http://www.itagsoftware.com/iTagInstaller.481.exe'
+Install-ChocolateyPackage 'itag' 'exe' '/S' 'http://www.itagsoftware.com/iTagInstaller.483.exe'
