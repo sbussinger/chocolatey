f827c7611d4a88c18a364b84b7d6e7873e14dee0
Scott Bussinger <scottb@bussinger.org>
2012-06-26 15:45:20 -0700

    Initial version of Snoop 2.7.1 installation

---
+++ snoop/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+try {
+  # This doesn't work because you can't download from codeplex directly
+  #Install-ChocolateyZipPackage 'snoop' 'http://snoopwpf.codeplex.com/downloads/get/375553' "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+  # Create a desktop link to snoop executable
+  $parentfolder = Split-Path -parent $MyInvocation.MyCommand.Definition
+  $target = Join-Path $parentfolder 'snoop.exe'
+  Install-ChocolateyDesktopLink $target
+
+  # the following is all part of error handling
+  Write-ChocolateySuccess 'snoop'
+} catch {
+  Write-ChocolateyFailure 'snoop' "$($_.Exception.Message)"
+  throw
+}
