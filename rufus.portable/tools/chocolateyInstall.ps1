3f0c593088db899039bc4a1ee1b1440bcf0c888e
Scott Bussinger <scottb@opto-pps.com>
2014-10-01 14:51:26 -0700

    Updated to Rufus 1.4.10

--- rufus.portable/tools/chocolateyInstall.ps1
+++ rufus.portable/tools/chocolateyInstall.ps1
@@ -1,3 +1,3 @@
 $scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 $destinationPath = Join-Path $scriptPath 'rufus.exe'
-Get-ChocolateyWebFile 'rufus.portable' "$destinationPath" 'http://rufus.akeo.ie/downloads/rufus_v1.3.4.exe'
+Get-ChocolateyWebFile 'rufus.portable' "$destinationPath" 'http://rufus.akeo.ie/downloads/rufus-1.4.10.exe'
