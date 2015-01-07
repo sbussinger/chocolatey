648ec9d369bef9b2253e8bd6d5bc84ed3a21a896
Scott Bussinger <scottb@bussinger.org>
2015-01-07 08:06:01 -0700

    Deprecating the rufus.portable package
    rufus.portable has been replaced by the rufus package by another
    maintainer

--- rufus.portable/tools/chocolateyInstall.ps1
+++
@@ -1,3 +0,0 @@
-$scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
-$destinationPath = Join-Path $scriptPath 'rufus.exe'
-Get-ChocolateyWebFile 'rufus.portable' "$destinationPath" 'https://rufus.akeo.ie/downloads/rufus-1.4.12.exe'
